b12f3640f17848e338f70fc0b3729ed1b4886894
Damodar <damodar.namala@outlook.com>
2022-08-21 13:15:20 +0530

    Initial Commit

---
+++ TestActions/TestActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestActionsApp.swift
+//  TestActions
+//
+//  Created by Damodar Namala on 21/08/22.
+//
+
+import SwiftUI
+
+@main
+struct TestActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
