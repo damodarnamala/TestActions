5bddeaad3191f619664b5f7f78e487e003a2fe3b
Damodar <damodar.namala@outlook.com>
2022-08-21 13:46:38 +0530

    updated

--- TestActions/ContentView.swift
+++ TestActions/ContentView.swift
@@ -10,10 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
 <<<<<<< HEAD
+<<<<<<< HEAD
         Text("Here is login file")
 =======
         Text("Here is login")
 >>>>>>> 0c70174 (login feature)
+=======
+        Text("Here is login file")
+>>>>>>> 1246eb8 (updated)
             .padding()
     }
 }
